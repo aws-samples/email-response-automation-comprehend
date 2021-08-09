670f12807d3021d734e731b04c606d09299c4d94
Ariyawansa <sariyawa@147dda45096e.ant.amazon.com>
2021-08-09 09:39:59 -0600

    Email classification workflow added.

---
+++ lambda/classify-emails-lambda/lambda_function.py
@@ -0,0 +1,127 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+import boto3
+import logging
+import os
+import json 
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+comprehend_client = boto3.client('comprehend')
+sns_client = boto3.resource('sns')
+ses_client = boto3.client('ses')
+
+
+endpoint_name = os.getenv("EMAIL_CLASSIFICATION_ENDPOINT_ARN")
+threashold = os.getenv("EMAIL_CLASSIFICATION_THREASHOLD")
+human_workflow_topic_arn = os.getenv("HUMAN_WORKFLOW_SNS_TOPIC_ARN")
+source_email = os.getenv("SOURCE_EMAIL")
+
+
+if not endpoint_name:
+   raise ValueError("env variable EMAIL_CLASSIFICATION_ENDPOINT_ARN is required.")
+   
+if not human_workflow_topic_arn:
+   raise ValueError("env variable HUMAN_WORKFLOW_SNS_TOPIC is required.")  
+   
+if not source_email:
+   raise ValueError("env variable SOURCE_EMAIL is required.")      
+   
+if not threashold:
+   threashold = 0.6
+   
+human_workflow_topic = sns_client.Topic(human_workflow_topic_arn)
+
+def call_endpoint(email_body):
+   
+   response = comprehend_client.classify_document(
+    Text=email_body,
+    EndpointArn=endpoint_name
+   )
+   
+   return response
+   
+   
+def best_class(results):
+   return max(results['Classes'], key=lambda x : x['Score'])
+   
+   
+def is_good_enough(best_result):
+   return best_result['Score'] >= threashold
+   
+def send_user_email(user_email, template_name):
+   
+   response = ses_client.send_templated_email(
+     Source=source_email,
+     Destination={
+       'ToAddresses': [
+         user_email,
+       ]
+     },
+     Template=template_name,
+     TemplateData={
+        
+     }
+   )
+   
+
+def send_to_human_workflow_topic(event):
+   
+   human_workflow_topic.publish(
+      Message = json.dumps(event, indent = 4)  ,
+      Subject = "Human workflow entry found"
+   )
+   
+def validate_params(event):
+   
+   email = event['email']
+   meta = event['meta']
+   
+   if not email :
+      raise ValueError("No email found to classify.")
+   if not meta :
+      raise ValueError("No metadata found.")
+   
+   email_body = email['body']
+   email_subject = email['subject']
+   user_email = email['to']
+   message_source = meta['source']
+   message_id = meta['id']
+   
+   
+   if not email_body:
+      raise ValueError("No email body found to classify.")
+   if not email_subject:
+      raise ValueError("No email subject found.")
+   if not user_email:
+      raise ValueError("No user email found.")
+   if not message_source:
+      raise ValueError("No email source found.")
+   if not message_id:
+      raise ValueError("No email source id found.")
+      
+   return email, meta
+
+def lambda_handler(event, context):
+ 
+   email, meta = validate_params(event)   
+   
+   logger.info("Executing the email classification lambda function with email content {}".format(email['body']))
+   
+   results = call_endpoint(email['body'])
+   
+   logger.info("Classification results returned. {}".format(results))
+   
+   best_intent = best_class(results)
+   
+   if(is_good_enough(best_intent)):
+      logger.info("Classification passed the threashold. Best matched intent is [{}]".format(best_intent['name']))
+      send_user_email(event, best_intent['Name'])
+   else:
+      send_to_human_workflow_topic(event)
+      
+   return best_intent
+  
+       
---
+++ stacks/email_classification_workflow_stack.py
@@ -0,0 +1,120 @@
+from aws_cdk import (
+    aws_lambda as lambda_,
+    aws_iam as iam,
+    aws_sns as sns,
+    aws_sns_subscriptions as subs,
+    aws_dynamodb as dynamodb,
+    core
+)
+
+import os.path as path
+
+
+class EmailClassificationWorkflowStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        human_topic = self.human_workflow_topic()
+
+        classification_lambda = self.classify_email_lambda(human_topic)
+        
+        workmail_lambda = self.workmail_integration_lambda(classification_lambda)
+        
+        #email_templates_dynamodb = self.email_templates_dynamodb() 
+        
+    
+    def workmail_integration_lambda(self, classification_lambda):
+        
+        # workmail_arn = self.node.try_get_context("workmail_arn")
+        # if not workmail_arn:
+        #      raise ValueError("Please provide arn of the workmail domain. You can provide that by specifying workmail_arn context variable")
+        
+        workmail_lambda = lambda_.Function(
+            self, "id_workmail_integration_lambda_lambda_fn", 
+            function_name="workmail-integration-lambda-fn",
+            code = lambda_.Code.from_asset(path.join("./lambda", "workmail-integration-lambda")),
+            handler = "lambda_function.lambda_handler",
+            runtime = lambda_.Runtime.PYTHON_3_8,
+            timeout = core.Duration.minutes(1),
+            environment={
+                "EMAIL_CLASSIFICATION_LAMBDA_FN_NAME" : classification_lambda.function_name
+            }
+        )
+        
+        principal = iam.ServicePrincipal("workmail.us-west-2.amazonaws.com")
+        workmail_lambda.grant_invoke(principal)
+        
+        workmail_lambda.add_to_role_policy(
+            iam.PolicyStatement(
+                        actions = [
+                            "workmailmessageflow:GetRawMessageContent",
+                        ],
+                        resources= [ '*' ]
+                    )
+            
+        )
+        
+        classification_lambda.grant_invoke(workmail_lambda)
+        
+        return workmail_lambda
+        
+        
+    def classify_email_lambda(self, human_workflow_topic):
+        
+        email_classification_endpoint_arn = self.node.try_get_context("email_classification_endpoint_arn")
+        if not email_classification_endpoint_arn:
+             raise ValueError("Please provide email classification endpoint. You can provide that by specifying email_classification_endpoint_arn context variable")
+        
+        email_classify_lambda = lambda_.Function(
+            self, "id_classify_emails_lambda_fn", 
+            function_name="classify-emails-lambda-fn",
+            code = lambda_.Code.from_asset(path.join("./lambda", "classify-emails-lambda")),
+            handler = "lambda_function.lambda_handler",
+            runtime = lambda_.Runtime.PYTHON_3_8,
+            timeout = core.Duration.minutes(1),
+            environment={
+                "EMAIL_CLASSIFICATION_ENDPOINT_ARN" : email_classification_endpoint_arn,
+                "HUMAN_WORKFLOW_SNS_TOPIC_ARN" : human_workflow_topic.topic_arn,
+                "SOURCE_EMAIL" : "sariyawa@amazon.com"
+            }
+        )
+        
+        email_classify_lambda.add_to_role_policy(
+            iam.PolicyStatement(
+                        actions = [
+                            "comprehend:*",
+                        ],
+                        resources= [ email_classification_endpoint_arn ]
+                    )
+            
+        )
+        
+        human_workflow_topic.grant_publish(email_classify_lambda)
+        
+        return email_classify_lambda
+        
+            
+    def human_workflow_topic(self):
+        
+        human_workflow_email = self.node.try_get_context("human_workflow_email")
+       
+        if not human_workflow_email:
+             raise ValueError("Please provide email to forward the human workflow events. You can provide that by specifying human_workflow_email context variable")
+             
+        topic =  sns.Topic(
+            self, "id_human_workflow_topic",
+            display_name="Email-classification-human-workflow-topic",
+            topic_name="Email-classification-human-workflow-topic"
+        )
+        
+        topic.add_subscription(subs.EmailSubscription(human_workflow_email))
+        
+        return topic
+        
+    def email_templates_dynamodb(self):
+        return dynamodb.Table(
+            self, "id_email_templates_db",
+            partition_key=dynamodb.Attribute(name="id", type=dynamodb.AttributeType.STRING)
+        )
+        
