18e29f054b20a3cc6d46c5b30bb696dc8873c584
Godwin Sahayaraj Vincent <vgodwin@amazon.com>
2021-08-11 12:33:55 -0600

    updating CFN output for s3 bucket  and upadting MD file and notebook file to mention that comprehend not available in us-west-1

--- nb_lifecycle/lifecycle.sh
+++ nb_lifecycle/lifecycle.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
-nohup pip install --upgrade pip
 
+nohup pip install --upgrade pip
 
 cd /home/ec2-user/SageMaker
 
 aws s3 cp s3://S3URL/comprehend_class_train_deploy_notebook.ipynb .
-
-#aws s3 cp S3URL .
