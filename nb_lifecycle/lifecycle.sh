048ccba3be8542bc5f56853fc12d29f45b3e0f68
Godwin Sahayaraj Vincent <vgodwin@amazon.com>
2021-08-17 22:20:45 -0600

    added changes on dataset

--- nb_lifecycle/lifecycle.sh
+++ nb_lifecycle/lifecycle.sh
@@ -5,4 +5,4 @@ nohup pip install --upgrade pip
 
 cd /home/ec2-user/SageMaker
 
-aws s3 cp s3://S3URL/comprehend_class_train_deploy_notebook.ipynb .
+aws s3 cp s3://S3URL/comprehend_class_entity_train_deploy_notebook.ipynb .
