ec0b874917e4073186a2501a9d430fc594490954
Godwin Sahayaraj Vincent <vgodwin@amazon.com>
2021-08-20 00:56:18 -0600

    email template added

--- lambda/classify-emails-lambda/mock_apis.py
+++ lambda/classify-emails-lambda/mock_apis.py
@@ -1,6 +1,6 @@
 import random
 
-TRN_STATUSES = ['FAILED', 'INPROGRESS', 'INVALID','PROCESSED']
+TRN_STATUSES = ['FAILED', 'INPROGRESS', 'INVALID','SUCCESSFUL']
 
 def get_trasfer_status(trn_id):
     return TRN_STATUSES[random.randint(0, len(TRN_STATUSES)-1)]
